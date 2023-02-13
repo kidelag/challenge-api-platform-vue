625f9800370c6bf73314191460894a5d5a10e70a
matthieu <campagne.matthieu@gmail.com>
2023-02-13 23:24:35 +0100

    Fix CI & mails

--- api/src/EventSubscriber/UserMailSubscriber.php
+++ api/src/EventSubscriber/UserMailSubscriber.php
@@ -53,7 +53,7 @@ class UserMailSubscriber implements EventSubscriberInterface
         $this->manager->persist($user);
         $this->manager->flush();
 
-        $link = 'http://localhost:8080/validate/' . $user->getToken();
+        $link = 'http://learn.matthieucmp.eu/validate/' . $user->getToken();
         $mail = (new Email())
             ->to($user->getMail())
             ->from('no-reply@challenge.fr')
